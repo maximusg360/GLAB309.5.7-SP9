b8b25dc9aa74676117a605a50ccd6d5135aedb38
Guzman <francisco.guzman@accenture.com>
2024-08-27 16:31:17 -0400

    lab done

---
+++ src/main/java/com/SessionAttributes/SessionAttributesApplication.java
@@ -0,0 +1,13 @@
+package com.SessionAttributes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionAttributesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionAttributesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SessionAttributes/SessionAttributesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SessionAttributes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionAttributesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
